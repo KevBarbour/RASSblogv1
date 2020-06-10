eb15bd21464e4fe1c200c70e56107b4ea325c9d6
Kevin Barbour <kevbarbour@gmail.com>
2020-06-10 17:15:55 +0100

    v1.1
    Fixed responsive Navbar with Jquery, cleaned up some css

--- mysite/static/js/mysite-jquery.js
+++ mysite/static/js/mysite-jquery.js
@@ -1,10 +1,43 @@
-(document).ready(function() {
-jQuery(document).ready(function() {
-    jQuery('.toggle-nav').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.menu ul').toggleClass('active');
 
-        e.preventDefault();
-    });
+/*hide show
+html:
+<button id="hide">Hide</button>
+<button id="show">Show</button>
+
+
+       $(document).ready(function(){
+  $("#hide").click(function(){
+    $(".menu").hide();
+  });
+  $("#show").click(function(){
+    $(".menu").show();
+  });
 });
+*/
+
+/*toggle v2
+$(document).ready(function(){
+    $(".menu").hide();
+    });
+*/
+
+$(document).ready(function(){
+
+/*hides toggle menu when resized */
+var resizeTimer;
+$(window).on('resize', function (e) {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function () {
+        if ($(window).width() > 860) {
+            $('.menu').show();
+        } else {
+            $('.menu').hide();
+        }
+    }, 250);
+});
+
+  $(".toggle-nav").click(function(){
+      $(".menu").toggle();
+
+  });
 });
--- mysite/staticfiles/js/mysite-jquery.js
+++ mysite/staticfiles/js/mysite-jquery.js
@@ -1,10 +1,23 @@
-(document).ready(function() {
+$(document).ready(function() {
 jQuery(document).ready(function() {
     jQuery('.toggle-nav').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.menu ul').toggleClass('active');
+        jQuery(this).toggleClass('active1');
+        jQuery('.menu ul').toggleClass('active1');
 
         e.preventDefault();
     });
 });
+        // JQuery code to be added in here.
 });
+
+
+
+/*
+
+try when fix the button it dissapears the list
+
+
+jQuery('.toggle-nav, .menu a').click(function(e) {
+    jQuery('.toggle-nav').toggleClass('active');
+    jQuery('.menu ul').toggleClass('active');
+e.preventDefault(); });*/
